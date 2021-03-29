4fd02a2609577c63e7b176d6625b6ea31421d540
Joe Knockenhauer <joe@knocknet.net>
2021-03-29 07:34:44 -0400

    create example file

---
+++ example-start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Helpful to read output when debugging
+set -x
+
+# Stop display manager
+systemctl stop display-manager.service
+## Uncomment the following line if you use GDM
+#killall gdm-x-session
+
+# Unbind VTconsoles
+echo 0 > /sys/class/vtconsole/vtcon0/bind
+echo 0 > /sys/class/vtconsole/vtcon1/bind
+
+# Unbind EFI-Framebuffer
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+# Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
+sleep 2
+
+# Unbind the GPU from display driver
+virsh nodedev-detach pci_0000_0c_00_0
+virsh nodedev-detach pci_0000_0c_00_1
