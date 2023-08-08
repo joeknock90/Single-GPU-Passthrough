e75d55b17c5e7acb9b0c61050d7a357272631cdb
Tamim Mahmud Khan 　( タミム・マフムド・カン) <30326616+tamimmahmudkhan@users.noreply.github.com>
2023-08-08 10:34:01 +0600

    Update and rename example-revert.sh to revert.sh
    Corrected script

---
+++ revert.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Helpful to read output when debugging
+set -x
+
+# Stop display manager
+systemctl stop display-manager.service
+## Uncomment the following line if you use GDM
+#killall gdm-x-session
+
+# Unbind VTconsoles
+echo 0 > /sys/class/vtconsole/vtcon0/bind
+echo 0 > /sys/class/vtconsole/vtcon1/bind
+
+# Unbind EFI-Framebuffer
+echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+# Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
+sleep 2
+
+# Unbind the GPU from display driver
+virsh nodedev-detach pci_0000_01_00_0  #Replace numbers with your specific pci id. Use lspci -nnk
+virsh nodedev-detach pci_0000_01_00_1  # This one too
+
+# Load VFIO Kernel Module  
+modprobe vfio-pci  
